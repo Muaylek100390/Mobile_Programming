a0c1cee4167da919ef6a6958c021dd07b4cf513e
a10305-5 <123>
2018-11-20 11:00:16 +0700

    Initial commit

---
+++ app/src/main/java/fahsai/ruts/ac/th/lab1/Lab1_Basic.java
@@ -0,0 +1,13 @@
+package fahsai.ruts.ac.th.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab1_Basic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab1__basic);
+    }
+}
